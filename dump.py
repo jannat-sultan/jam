08950394b82e00ae0f2a8d078b5f4a9868661bbc
jannat <83273708+jannat-sultan@users.noreply.github.com>
2023-03-05 22:18:48 +0500

    Update dump.py

--- dump.py
+++ dump.py
@@ -34,31 +34,56 @@ def Jadu():
 			main()
 
 def login():
-	os.system("clear")
-	print(logo)
-	now = datetime.datetime.now()
-	print("Date and Time ")
-	print(now.strftime("%y-%m-%d %H:%M:%S"))
-	hostnm = socket.gethostname()
-	ipaddress = socket.gethostbyname(hostnm)
-	print("ip address is :", ipaddress)
-	print("country : Pakistan")
-	print("_____________________________________")
-	try:
-		fbcokis= input('[>>] Input Your Facebook Cookie : ')
-		print(47*"\033[1;37;1m-")
-		head = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}
-		ftoken = requests.get("https://business.facebook.com/business_locations", headers=head, cookies = {"cookie":fbcokis}).text
-		eaag = re.search("(EAAG\w+)",str(ftoken))
-		open("data/token.txt", "w").write(eaag.group(1))
-		open("data/cookie.txt", "w").write(fbcokis)
-		token = open('data/token.txt','r').read()
-		main()
-	except Exception as e:
-		os.system("rm -f data/token.txt")
-		print(f"[*] Error {e}")
-		slp(2)
-		login()
+    mkdir_data_login()
+    print(logo)
+    print('\n%s[%s•%s] %sInput Cookies %s!'%(M,P,M,P,M))
+    cookie = str(input('%s[%s•%s] %sCookies %s: %s'%(J,P,J,P,J,P)))
+    try:
+        token = clotox(cookie)
+        coki = {'cookie':cookie} 
+        open('login/cookie.txt','w').write(cookie)
+        open('login/token.txt','w').write(token)
+        jam()
+    except requests.exceptions.ConnectionError:print('\n   %s[%s•%s] %sNo Internet %s!%s\n'%(M,P,M,P,M,P));exit()
+    except (KeyError,IOError,AttributeError):print('\n   %s[%s•%s] %sCookies Invalid %s!%s\n'%(M,P,M,P,M,P));exit()
+
+def mkdir_data_login():
+    # Make Directory Login Data
+    try:os.mkdir("login")
+    except:pass
+    # Make Directory Dump
+    try:os.mkdir("dump")
+    except:pass
+    # Delete Cookies
+    try:os.remove('login/cookie.txt')
+    except:pass
+    # Delete Token
+    try:os.remove('login/token.txt')
+    except:pass
+
+
+def clotox(cookie):
+    with requests.Session() as xyz:
+        get_tok = xyz.get(url_businness+'/business_locations',headers = {
+            "user-agent":ua_business,
+            "referer": web_fb,
+            "host": "business.facebook.com",
+            "origin": url_businness,
+            "upgrade-insecure-requests" : "1",
+            "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "max-age=0",
+            "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "content-type":"text/html; charset=utf-8"},cookies = {"cookie":cookie})
+        return (re.search("(EAAG\w+)", get_tok.text).group(1))
+
+url_businness = "https://business.facebook.com"
+ua_business = "Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36"
+kata_dev = 'Lu Ganteng Banget Bang. Gw Mau Recode SClu, Soalnya Gw Goblok Soal Coding'
+web_fb = "https://www.facebook.com/"
+m_fb = "https://m.facebook.com/"
+mbasic = "https://mbasic.facebook.com/"
+header_grup = {"user-agent": "Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]"}
+###
 def grep(f):
 	o = input('\033[0;97m[->] Save As : \033[1;32;1m')
 	print(47*"\033[1;37;1m-")
